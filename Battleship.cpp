4af4769856fd6861052106363cc972d8188d11c4
William Turkett <5298611+turketwh@users.noreply.github.com>
2020-01-28 11:09:21 -0500

    Initial commit

---
+++ Battleship.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*
+ *  CSC 112: Project 1: Battleship (Part 2)
+ *  Name: William Turkett
+ *  Date: January 23, 2020
+ */
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}

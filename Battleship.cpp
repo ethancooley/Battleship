f0e2c22b3da6be321df7bfd08b6e4fa0f03ca852
ethancooley <coolef17@wfu.edu>
2020-01-28 19:32:22 -0500

    Step 2 Submission

--- Battleship.cpp
+++ Battleship.cpp
@@ -1,13 +1,106 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 /*
  *  CSC 112: Project 1: Battleship (Part 2)
  *  Name: William Turkett
  *  Date: January 23, 2020
+ *  Author: Ethan Cooley
  */
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    // Initialize variables //
+
+    srand(time(0));  // Seed
+    int Guesses = 0; // Guesses counter starts at 0
+    int N; // Initialize integer N for board size
+
+    bool Board = false; // Board will be false until a positive NxN is established
+    while (!Board) {    // Game will ask for board size again if input is negative
+
+        cout << "Enter N for NxN Board Size: ";
+        cin >> N;
+        if (N < 1) {
+            cout << "Please enter a positive number for board size." << endl;
+            cout << endl;
+        }
+        else {
+            Board = true; // Break the loop if board size is positive
+        }
+    }
+
+    string MapArray[N][N]; // Initialize map array
+
+    // Replace empty spaces in array with " "
+    int i;
+    int j;
+    for (i = 0; i < N; i++) {   // Nested Loop Action
+        for (j = 0; j < N; j++) {
+            MapArray[i][j] = " ";
+        }
+    }
+
+    // Randomly generate battleship location
+    int ship_row = (rand() % N) + 1;
+    int ship_col = (rand() % N) + 1;
+
+    // Game will run as long as battleship is not hit
+    bool Sink = false;
+
+    // Main Loop
+    while (!Sink) {
+        // Display Location options
+        cout << "Location (row[1-" << N << "] column[1-" << N << "]): ";
+
+        int row; // Initialize Variables
+        int col;
+        cin >> row; // User Input
+        cin >> col;
+
+        // If input is negative
+        if ((row < 1) || (col < 1)) {
+            cout << "Location must be positive." << endl;   // Sanitize input
+            cout << endl;
+            continue;                                       // Ask again for input
+        }
+
+        // If position is invalid
+        if ((row > N) || (col > N)) {
+            cout << "Invalid Position." << endl;
+            cout << endl;
+            continue;
+        }
+
+        Guesses += 1; // Guess counter increases by 1 each time a user inputs
+                      // * Only if the input is valid
+
+        if (row == ship_row && col == ship_col) {     // If the user hits the battleship
+            MapArray[row - 1][col - 1] = "O";         // Location turns to O
+        } else if ((row <= N) && (col <= N)) {        // If user does not hit battleship
+            MapArray[row - 1][col - 1] = "X";         // And position is valid, location turns to X
+        }
+
+        // Display Map
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                if (j == N-1) {
+                    cout << MapArray[i][j] << endl;
+                }
+                else {
+                    cout << MapArray[i][j] << "!";
+                }
+            }
+        }
+        cout << " " << endl;
+
+        // If the user hits the battleship
+        if (MapArray[row - 1][col - 1] == "O") {
+            cout << "You sunk my battleship! (" << Guesses << " guesses)" << endl;
+            Sink = true;
+        }
+    }
     return 0;
 }
